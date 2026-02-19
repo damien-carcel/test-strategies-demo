aaeec28b7bddf0446c4a5239221e779690a090b8
Damien Carcel <damien.carcel@akeneo.com>
2026-02-19 15:05:40 +0100

    feat: user's fields cannot be empty

--- src/Domain/Model/User.php
+++ src/Domain/Model/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Domain\Model;
 
+use App\Domain\Exception\InvalidUser;
 use Symfony\Component\Uid\Uuid;
 
 class User
@@ -9,19 +10,30 @@ class User
     public private(set) Uuid $id {
         get => $this->id;
     }
+    public private(set) string $email {
+        get => $this->email;
+    }
+    public private(set) string $firstname {
+        get => $this->firstname;
+    }
+    public private(set) string $lastname {
+        get => $this->lastname;
+    }
 
-    public function __construct(
-        private(set) string $email {
-            get => $this->email;
-        },
-        private(set) string $firstname {
-            get => $this->firstname;
-        },
-        private(set) string $lastname {
-            get => $this->lastname;
-        },
-    )
+    public function __construct(string $email, string $firstname, string $lastname)
     {
+        $emptyFields = array_filter(
+            ['email' => $email, 'firstname' => $firstname, 'lastname' => $lastname],
+            static fn (string $field): bool => '' === $field,
+            ARRAY_FILTER_USE_BOTH,
+        );
+        if (!empty($emptyFields)) {
+            throw new InvalidUser(...array_keys($emptyFields));
+        }
+
         $this->id = Uuid::v7();
+        $this->email = $email;
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
     }
 }
--- tests/Unit/Entity/UserTest.php
+++ tests/Unit/Entity/UserTest.php
@@ -2,7 +2,9 @@
 
 namespace App\Tests\Unit\Entity;
 
+use App\Domain\Exception\InvalidUser;
 use App\Domain\Model\User;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Uid\Uuid;
@@ -39,5 +41,65 @@ final class UserTest extends TestCase
         $user = new User('john.doe@email.com', 'John', 'Doe');
 
         self::assertSame('Doe', $user->lastname);
+    }
+
+    #[Test]
+    #[DataProvider('emptyProperties')]
+    public function itCannotBeCreatedWithEmptyProperties(
+        string $email,
+        string $firstname,
+        string $lastname,
+        string $expectedExceptionMessage,
+    ): void {
+        $this->expectException(InvalidUser::class);
+        $this->expectExceptionMessage($expectedExceptionMessage);
+
+        new User($email, $firstname, $lastname);
+    }
+
+    /**
+     * @return iterable<string, array{
+     *     email: string,
+     *     firstname: string,
+     *     lastname: string,
+     *     expectedExceptionMessage: string,
+     * }>
+     */
+    public static function emptyProperties(): iterable
+    {
+        yield 'empty email' => [
+            'email' => '',
+            'firstname' => 'John',
+            'lastname' => 'Doe',
+            'expectedExceptionMessage' => "The user's email cannot be empty.",
+        ];
+
+        yield 'empty firstname' => [
+            'email' => 'john.doe@email.com',
+            'firstname' => '',
+            'lastname' => 'Doe',
+            'expectedExceptionMessage' => "The user's firstname cannot be empty.",
+        ];
+
+        yield 'empty lastname' => [
+            'email' => 'john.doe@email.com',
+            'firstname' => 'John',
+            'lastname' => '',
+            'expectedExceptionMessage' => "The user's lastname cannot be empty.",
+        ];
+
+        yield 'multiple empty properties' => [
+            'email' => 'john.doe@email.com',
+            'firstname' => '',
+            'lastname' => '',
+            'expectedExceptionMessage' => "The user's firstname and lastname cannot be empty.",
+        ];
+
+        yield 'all properties empty' => [
+            'email' => '',
+            'firstname' => '',
+            'lastname' => '',
+            'expectedExceptionMessage' => "The user's email, firstname and lastname cannot be empty.",
+        ];
     }
 }
